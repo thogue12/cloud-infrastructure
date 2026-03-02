15121d3ed4b4201ccd5eefdd846dd550c9cb856b
thogue12 <hoguetimothy12@gmail.com>
2026-03-01 21:20:07 -0500

    another commit

--- Azure/modules/key_vault/main.tf
+++ Azure/modules/key_vault/main.tf
@@ -9,9 +9,15 @@ resource "azurerm_key_vault" "this_key_vault" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = var.purge_protection_enabled
+  purge_protection_enabled    = true
+  public_network_access_enabled = false
 
   sku_name = var.sku_name
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
 
 }
 resource "azurerm_role_assignment" "key_vault_admin" {
--- Azure/modules/sql_server/main.tf
+++ Azure/modules/sql_server/main.tf
@@ -59,4 +59,3 @@ resource "azurerm_mssql_database" "this_db" {
     prevent_destroy = false
   }
 }
-
