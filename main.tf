e81509332b8e13d5b2f3bdc344d5e7f302b65167
Timothy hogue <104541670+thogue12@users.noreply.github.com>
2026-05-05 23:26:31 -0400

    Enable Azure AD storage usage in provider config

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   subscription_id = var.azure_subscription_id
+  storage_use_azuread = true
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
