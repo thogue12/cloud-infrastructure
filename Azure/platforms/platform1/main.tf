085841226c7cba838c2c6f7cafffd52ae17cba04
Timothy hogue <104541670+thogue12@users.noreply.github.com>
2026-05-05 21:18:10 -0400

    Update module sources to use new directory structure

--- Azure/platforms/platform1/main.tf
+++ Azure/platforms/platform1/main.tf
@@ -8,7 +8,7 @@ resource "random_string" "suffix" {
 }
 
 module "resource_group" {
-  source      = "../resource_group"
+  source      = "../../modules/resource_group"
   name        = local.resource_group_name
   location    = var.location
   environment = var.environment
@@ -16,7 +16,7 @@ module "resource_group" {
 }
 
 module "virtual_network" {
-  source             = "../../virtual_network"
+  source             = "../../modules/virtual_network"
   vnet_name          = local.vnet_name
   vnet_address       = var.vnet_address
   location           = module.resource_group.location
@@ -29,7 +29,7 @@ module "virtual_network" {
 }
 
 module "sql_server" {
-  source               = "../../sql_server"
+  source               = "../../modules/sql_server"
   sql_server_name      = local.sql_server_name
   admin_login          = local.admin_login
   admin_login_password = var.admin_login_password
@@ -42,7 +42,7 @@ module "sql_server" {
 }
 
 module "app_service_plan" {
-  source             = "../../app_service_plan"
+  source             = "../../modules/app_service_plan"
   location           = module.resource_group.location
   name               = module.resource_group.resource_group_name
   web_app_name       = local.web_app_name
@@ -56,7 +56,7 @@ module "app_service_plan" {
 }
 
 module "key_vault" {
-  source                   = "../../key_vault"
+  source                   = "../../modules/key_vault"
   purge_protection_enabled = local.purge_protection_enabled
   key_vault_name           = local.key_vault_name
   sku_name                 = local.sku_name
@@ -69,7 +69,7 @@ module "key_vault" {
 # Storage Account
 ###############################################################################################################################
 module "storage_account" {
-  source                = "../../storage_account"
+  source                = "../../modules/storage_account"
   contianer_access_type = local.contianer_access_type
   contianer_name        = local.contianer_name_1
   storage_account_name  = local.storage_account_name_1
@@ -78,7 +78,7 @@ module "storage_account" {
 }
 
 module "storage_account2" {
-  source                = "../../storage_account"
+  source                = "../../modules/storage_account"
   contianer_access_type = local.contianer_access_type
   contianer_name        = local.container2_name
   storage_account_name  = local.storage_account2_name #storage account name must be globally unique and no 'dashes -' allowed
@@ -87,7 +87,7 @@ module "storage_account2" {
 }
 
 module "function_app" {
-  source                     = "../../function_app"
+  source                     = "../../modules/function_app"
   name                       = module.resource_group.resource_group_name
   location                   = module.resource_group.location
   function_app_name          = local.function_app_name
@@ -98,7 +98,7 @@ module "function_app" {
 }
 
 module "web_app" {
-  source                  = "../../web_app"
+  source                  = "../../modules/web_app"
   name                    = module.resource_group.resource_group_name
   location                = module.resource_group.location
   web_app_service_plan_id = module.app_service_plan.web_app_service_plan_id
