a9d8a7192b8454035ecd2b607863c5dfbb77af15
Timothy hogue <104541670+thogue12@users.noreply.github.com>
2026-05-05 21:23:53 -0400

    Comment out variable definitions for AKS and VNet
    Comment out several variable definitions in variables.tf

--- variables.tf
+++ variables.tf
@@ -25,35 +25,35 @@ variable "subnet_address" {
   type        = list(string)
 }
 
-variable "aks_subnet_address" {
-  description = "AKS subnet address space (must not overlap with subnet_address)"
-  type        = list(string)
-}
+# variable "aks_subnet_address" {
+#   description = "AKS subnet address space (must not overlap with subnet_address)"
+#   type        = list(string)
+# }
 
 variable "vnet_address" {
   description = "VNet address space"
   type        = list(string)
 }
 
-variable "node_count" {
-  description = "Number of nodes in the default node pool"
-  type        = number
-  default     = 2
-}
+# variable "node_count" {
+#   description = "Number of nodes in the default node pool"
+#   type        = number
+#   default     = 2
+# }
 
-variable "node_name" {
-  description = "Name of the default node pool"
-  type        = string
-  default     = "default"
-}
+# variable "node_name" {
+#   description = "Name of the default node pool"
+#   type        = string
+#   default     = "default"
+# }
 
 
-variable "should_delegate" {
-  type = bool
-  description = "Determine whether or not the subnet delegation should be created"
-}
+# variable "should_delegate" {
+#   type = bool
+#   description = "Determine whether or not the subnet delegation should be created"
+# }
 
-variable "enable_nat_gateway" {
-  type = bool
-  description = "Determine whether you need to create the NATGW"
-}
+# variable "enable_nat_gateway" {
+#   type = bool
+#   description = "Determine whether you need to create the NATGW"
+# }
