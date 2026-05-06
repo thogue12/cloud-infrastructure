94f6ce8a91171d30f60b9a9ef71fe15f33b50048
Timothy hogue <104541670+thogue12@users.noreply.github.com>
2026-05-05 20:59:33 -0400

    Uncomment and modify Terraform module configurations

--- main.tf
+++ main.tf
@@ -24,39 +24,39 @@ provider "azurerm" {
   }
 }
 
-module "platform2" {
-  source             = "./Azure/platforms/platform2"
-  project_name       = var.project_name
-  location           = var.location
-  subnet_address     = var.subnet_address
-  aks_subnet_address = var.aks_subnet_address
-  vnet_address       = var.vnet_address
-  environment        = var.environment
-  should_delegate    = var.should_delegate
-  enable_nat_gateway = var.enable_nat_gateway
-  node_count         = var.node_count
-}
+# module "platform2" {
+ # source             = "./Azure/platforms/platform2"
+ # project_name       = var.project_name
+ # location           = var.location
+ # subnet_address     = var.subnet_address
+ # aks_subnet_address = var.aks_subnet_address
+ # vnet_address       = var.vnet_address
+ # environment        = var.environment
+ # should_delegate    = var.should_delegate
+ # enable_nat_gateway = var.enable_nat_gateway
+ # node_count         = var.node_count
+#}
 
-# module "platform" {
-#   source               = "./Azure/modules/platforms/platform1"
-#   environment          = var.environment
-#   project_name         = var.project_name
-#   location             = var.location
-#   admin_login_password = var.admin_login_password
-#   subnet_address       = var.subnet_address
-#   vnet_address         = var.vnet_address
-#   client_name          = var.client_name
-#   enable_nat_gateway   = var.enable_nat_gateway
+ module "platform" {
+   source               = "./Azure/modules/platforms/platform1"
+   environment          = var.environment
+  project_name         = var.project_name
+   location             = var.location
+   admin_login_password = var.admin_login_password
+   subnet_address       = var.subnet_address
+  vnet_address         = var.vnet_address
+   client_name          = var.client_name
+   enable_nat_gateway   = var.enable_nat_gateway
 
 
 
-# }
+ }
 
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "tf_state"
-#     storage_account_name = ""
-#     container_name       = ""
-#     key                  = ""
-#   }
-# }
+ terraform {
+   backend "azurerm" {
+     resource_group_name  = "tf_state"
+     storage_account_name = ""
+     container_name       = ""
+     key                  = ""
+   }
+ }
